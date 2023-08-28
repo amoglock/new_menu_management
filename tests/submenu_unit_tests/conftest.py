58c9a588368d3ceddb87786afc539f88c86ccf82
amoglock <amoglock@gmail.com>
2023-08-28 17:54:16 +0400

    deleted conftest.py for submenu

--- tests/submenu_unit_tests/conftest.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-# from httpx import AsyncClient
-
-# @pytest.fixture
-# async def get_menu_id(ac: AsyncClient) -> str:
-#     response = await ac.get('/api/v1/menus/')
-#     menu_id = response.json()[0].get('id')
-#     return menu_id
