16000c2e9101d9fb4e5d8e0b48e0e017775846f7
amoglock <amoglock@gmail.com>
2023-09-07 14:39:12 +0400

    turn to class

--- src/config.py
+++ src/config.py
@@ -1,41 +1,23 @@
-import os
-
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
-REDIS_HOST = os.environ.get('REDIS_HOST')
 
-MODE = os.environ.get('MODE')
+class Settings(BaseSettings):
+
+    MODE: str
+
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+
+    @property
+    def db_url(self) -> str:
+        return (f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:'
+                f'{self.DB_PORT}/{self.DB_NAME}?async_fallback=True')
 
 
-def db_url():
-    return f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?async_fallback=True'
-
-
-def get_mode():
-    return MODE
-
-# class Settings(BaseSettings):
-#
-#     MODE: str
-#
-#     DB_NAME: str
-#     DB_HOST: str
-#     DB_PORT: int
-#     DB_USER: str
-#     DB_PASS: str
-#
-#     @property
-#     def db_url(self) -> str:
-#         return f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-#
-#     model_config = SettingsConfigDict(env_file=".env")
-#
-#
-# settings = Settings()
+settings = Settings()
