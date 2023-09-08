7a8f18bba8b5c7b4cfd27f22cf07f047baa489fa
amoglock <amoglock@gmail.com>
2023-09-08 23:47:59 +0400

    added Depends

--- src/menu_management/routers/submenu_router.py
+++ src/menu_management/routers/submenu_router.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from src.menu_management.schemas.schemas import (
     CreateSubmenu,
@@ -18,8 +18,8 @@ submenu_router = APIRouter(
 @submenu_router.get('/', response_model=list[SubmenuResponse], status_code=200,
                     description='Возвращает список подменю для определенного меню которое есть в базе, '
                                 'если подменю нет - возвращает пустой список', summary='получить список подменю')
-async def get_submenus(menu_id: str):
-    return await SubmenuService.get_all_submenus(menu_id)
+async def get_submenus(menu_id: str, submenu_service: SubmenuService = Depends()):
+    return await submenu_service.get_all_submenus(menu_id)
 
 
 @submenu_router.get('/{submenu_id}', response_model=SubmenuResponse, status_code=200,
@@ -27,27 +27,27 @@ async def get_submenus(menu_id: str):
                                 'Если submenu_id не найден - вызывается исключение с ошибкой 404',
                     summary='получить определенное подменю'
                     )
-async def get_specific_submenu(submenu_id: str):
-    return await SubmenuService.get_submenu(submenu_id)
+async def get_specific_submenu(submenu_id: str, submenu_service: SubmenuService = Depends()):
+    return await submenu_service.get_submenu(submenu_id)
 
 
 @submenu_router.post('/', response_model=SubmenuResponse, status_code=201,
                      description='Создает новое подменю. Принимает схему с названием и описанием и menu_id для привязки'
                                  'к существующему меню. Возвращает объект нового меню', summary='создать новое подменю')
-async def add_submenu(menu_id: str, new_submenu: CreateSubmenu):
-    return await SubmenuService.post_submenu(menu_id, new_submenu)
+async def add_submenu(menu_id: str, new_submenu: CreateSubmenu, submenu_service: SubmenuService = Depends()):
+    return await submenu_service.post_submenu(menu_id, new_submenu)
 
 
 @submenu_router.patch('/{submenu_id}', response_model=SubmenuResponse, status_code=200,
                       description='Изменяет существующее подменю. Принимает submenu_id для поиска и схему с новыми '
                                   'данными. Возвращает обновленный экземпляр. Если submenu_id не найден - '
                                   'вызывается исключение с ошибкой 404', summary='Изменить существующее подменю')
-async def patch_menu(submenu_id: str, new_submenu: PatchSubmenu):
-    return await SubmenuService.patch_submenu(submenu_id, new_submenu)
+async def patch_menu(submenu_id: str, new_submenu: PatchSubmenu, submenu_service: SubmenuService = Depends()):
+    return await submenu_service.patch_submenu(submenu_id, new_submenu)
 
 
 @submenu_router.delete('/{submenu_id}', response_model=dict[str, Union[str, bool]], status_code=200,
                        description='Удаляет существующее подменю. Принимает submenu_id для поиска.Возвращает '
                                    'словарь с информацией, что удаление совершено.', summary='удалить подменю')
-async def delete_menu(submenu_id: str, menu_id: str):
-    return await SubmenuService.delete(submenu_id, menu_id)
+async def delete_menu(submenu_id: str, menu_id: str, submenu_service: SubmenuService = Depends()):
+    return await submenu_service.delete(submenu_id, menu_id)
