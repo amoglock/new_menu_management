7d5c38e27454d687e538fff8279098e2a69b64d1
amoglock <amoglock@gmail.com>
2023-09-08 23:45:38 +0400

    added session

--- src/menu_management/repository/dish_repository.py
+++ src/menu_management/repository/dish_repository.py
@@ -1,79 +1,65 @@
-from fastapi import HTTPException
-from sqlalchemy import delete, func, insert, select, update
+from fastapi import Depends, HTTPException
+from sqlalchemy import delete, insert, select, update
 from sqlalchemy.exc import DBAPIError, IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.db import engine
+from src.database.db import get_session
 from src.database.models import Dish
-from src.menu_management.schemas.schemas import DishResponse
 
 
 class DishRepository:
 
-    @classmethod
-    async def get_dish_list(cls, submenu_id: str) -> list[DishResponse]:
+    def __init__(self, session: AsyncSession = Depends(get_session)):
+        self.session = session
+
+    async def get_dish_list(self, submenu_id: str) -> list[Dish]:
         stmt = select(Dish).where(Dish.submenu_group == submenu_id)
         try:
-            async with engine.connect() as conn:
-                result = await conn.execute(stmt)
-                return result
+            result = await self.session.execute(stmt)
+            return list(result.scalars().fetchall())
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def get_dish(cls, dish_id: str) -> DishResponse:
+    async def get_dish(self, dish_id: str) -> Dish:
         stmt = select(Dish).where(Dish.id == dish_id).limit(1)
         try:
-            async with engine.connect() as conn:
-                dish = await conn.execute(stmt)
-                dish = dish.fetchone()
-                return dish
+            dish = await self.session.execute(stmt)
+            dish = dish.scalar()
+            return dish
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def add_dish(cls, dish_as_dict: dict) -> DishResponse:
+    async def add_dish(self, dish_as_dict: dict) -> Dish:
         stmt = insert(Dish).values(**dish_as_dict).returning(Dish)
-        async with engine.connect() as conn:
-            async with conn.begin():
-                try:
-                    new_dish = await conn.execute(stmt)
-                    new_dish = new_dish.fetchone()
-                    return new_dish
-                except IntegrityError as e:
-                    raise HTTPException(status_code=409, detail=f'{e.orig}')
-                except DBAPIError as e:
-                    raise HTTPException(status_code=404, detail=f'{e.orig}')
-
-    @classmethod
-    async def update_dish(cls, dish_id: str, dish: dict) -> DishResponse:
-        stmt = update(Dish).where(Dish.id == dish_id).values(dish).returning(Dish)
         try:
-            async with engine.connect() as conn:
-                async with conn.begin():
-                    patched_dish = await conn.execute(stmt)
-                    patched_dish = patched_dish.fetchone()
-                    return patched_dish
+            new_dish = await self.session.execute(stmt)
+            await self.session.commit()
+            new_dish = new_dish.scalar()
+            return new_dish
         except IntegrityError as e:
             raise HTTPException(status_code=409, detail=f'{e.orig}')
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def delete_dish(cls, dish_id: str) -> dict[str, bool | str]:
-        stmt = delete(Dish).where(Dish.id == dish_id).returning(Dish)
+    async def update_dish(self, dish_id: str, dish: dict) -> Dish:
+        stmt = update(Dish).where(Dish.id == dish_id).values(dish).returning(Dish)
         try:
-            async with engine.connect() as conn:
-                async with conn.begin():
-                    deleted_dish = await conn.execute(stmt)
-                    if deleted_dish.fetchone():
-                        return {'status': True, 'message': 'The dish has been deleted'}
-                    return {'status': False, 'message': 'The dish not found'}
+            patched_dish = await self.session.execute(stmt)
+            await self.session.commit()
+            patched_dish = patched_dish.scalar()
+            return patched_dish
+        except IntegrityError as e:
+            raise HTTPException(status_code=409, detail=f'{e.orig}')
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def count(cls) -> int:
-        query = select(func.count()).select_from(Dish)
-        async with engine.connect() as conn:
-            menu_count = await conn.scalar(query)
-            return menu_count
+    async def delete_dish(self, dish_id: str) -> dict[str, bool | str]:
+        stmt = delete(Dish).where(Dish.id == dish_id).returning(Dish)
+        try:
+            deleted_dish = await self.session.execute(stmt)
+            await self.session.commit()
+            if deleted_dish.scalar():
+                return {'status': True, 'message': 'The dish has been deleted'}
+            return {'status': False, 'message': 'The dish not found'}
+        except DBAPIError as e:
+            raise HTTPException(status_code=404, detail=f'{e.orig}')
