8e68f4b6d6fcf67c68408a696147e0ebaef004a7
amoglock <amoglock@gmail.com>
2023-09-08 23:46:38 +0400

    added session

--- src/menu_management/repository/submenu_repository.py
+++ src/menu_management/repository/submenu_repository.py
@@ -1,77 +1,63 @@
-from fastapi import HTTPException
-from sqlalchemy import delete, func, insert, select, update
+from fastapi import Depends, HTTPException
+from sqlalchemy import delete, insert, select, update
 from sqlalchemy.exc import DBAPIError, IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.db import engine
+from src.database.db import get_session
 from src.database.models import Submenu
-from src.menu_management.schemas.schemas import SubmenuResponse
 
 
 class SubmenuRepository:
 
-    @classmethod
-    async def get_list_submenus(cls, menu_id: str) -> list[SubmenuResponse]:
+    def __init__(self, session: AsyncSession = Depends(get_session)):
+        self.session = session
+
+    async def get_list_submenus(self, menu_id: str) -> list[Submenu]:
         stmt = select(Submenu).filter_by(menu_group=menu_id)
         try:
-            async with engine.connect() as conn:
-                result = await conn.execute(stmt)
-                return result
+            result = await self.session.execute(stmt)
+            return list(result.scalars().fetchall())
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def get_submenu(cls, submenu_id: str):
-        query = select(Submenu).where(Submenu.id == submenu_id).limit(1)
+    async def get_submenu(self, submenu_id: str) -> Submenu:
+        query = select(Submenu).where(Submenu.id == submenu_id)
         try:
-            async with engine.connect() as conn:
-                result = await conn.execute(query)
-                result = result.fetchone()
-                return result
+            result = await self.session.execute(query)
+            result = result.scalar()
+            return result
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def add_submenu(cls, values: dict) -> SubmenuResponse:
+    async def add_submenu(self, values: dict) -> Submenu:
         stmt = insert(Submenu).values(**values).returning(Submenu)
-        async with engine.connect() as conn:
-            async with conn.begin():
-                try:
-                    new_submenu = await conn.execute(stmt)
-                    new_submenu = new_submenu.fetchone()
-                    return new_submenu
-                except IntegrityError:
-                    raise HTTPException(status_code=409, detail='This submenu title already exists')
-                except DBAPIError as e:
-                    raise HTTPException(status_code=404, detail=f'{e.orig}')
+        try:
+            new_submenu = await self.session.execute(stmt)
+            await self.session.commit()
+            new_submenu = new_submenu.scalar()
+            return new_submenu
+        except IntegrityError:
+            raise HTTPException(status_code=409, detail='This submenu already exists or wrong menu_id')
+        except DBAPIError as e:
+            raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def update_submenu(cls, submenu_id: str, submenu: dict) -> SubmenuResponse:
+    async def update_submenu(self, submenu_id: str, submenu: dict) -> Submenu:
         stmt = update(Submenu).where(Submenu.id == submenu_id).values(submenu).returning(Submenu)
         try:
-            async with engine.connect() as conn:
-                async with conn.begin():
-                    new_submenu = await conn.execute(stmt)
-                    new_submenu = new_submenu.fetchone()
-                    return new_submenu
+            new_submenu = await self.session.execute(stmt)
+            await self.session.commit()
+            new_submenu = new_submenu.scalar()
+            return new_submenu
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
 
-    @classmethod
-    async def delete_submenu(cls, take_id: str) -> dict[str, str | bool]:
+    async def delete_submenu(self, take_id: str) -> dict[str, str | bool]:
         stmt = delete(Submenu).where(Submenu.id == take_id).returning(Submenu)
         try:
-            async with engine.connect() as conn:
-                async with conn.begin():
-                    deleted_submenu = await conn.execute(stmt)
-                    if deleted_submenu.fetchone():
-                        return {'status': True, 'message': 'submenu has been deleted'}
-                    return {'status': False, 'message': 'submenu not found'}
+            deleted_submenu = await self.session.execute(stmt)
+            await self.session.commit()
+            if deleted_submenu.scalar():
+                return {'status': True, 'message': 'submenu has been deleted'}
+            return {'status': False, 'message': 'submenu not found'}
         except DBAPIError as e:
             raise HTTPException(status_code=404, detail=f'{e.orig}')
-
-    @classmethod
-    async def count(cls) -> int:
-        stmt = select(func.count()).select_from(Submenu)
-        async with engine.connect() as conn:
-            result = await conn.scalar(stmt)
-            return result
